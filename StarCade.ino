a2a6608e17d8cc1ba8d2e4ecf9e7b5822592ecf4
Scott Siebers <s.siebers@gmail.com>
2018-05-17 22:41:04 -0500

    Moved SoundFx to SimpleAnimation library

--- StarCade.ino
+++ StarCade.ino
@@ -1,38 +1,7 @@
 //Core Libraries
 #include <stdint.h>
-
-//FastLED
-#include <noise.h>
-#include <bitswap.h>
-#include <fastspi_types.h>
-#include <pixelset.h>
-#include <fastled_progmem.h>
-#include <led_sysdefs.h>
-#include <hsv2rgb.h>
-#include <fastled_delay.h>
-#include <colorpalettes.h>
-#include <color.h>
-#include <fastspi_ref.h>
-#include <fastspi_bitbang.h>
-#include <controller.h>
-#include <fastled_config.h>
-#include <colorutils.h>
-#include <chipsets.h>
-#include <pixeltypes.h>
-#include <fastspi_dma.h>
-#include <fastpin.h>
-#include <fastspi_nop.h>
-#include <platforms.h>
-#include <lib8tion.h>
-#include <cpp_compat.h>
-#include <fastspi.h>
-#include <FastLED.h>
-#include <dmx.h>
-#include <power_mgt.h>
-
-// Button Debounce
 #include <Bounce2.h>
-#include "SoundFx.h"
+#include "SimpleAnimation.h"
 
 
 
@@ -45,42 +14,55 @@
 #endif
 
 
+//Pins
+//Reserve pins 0 & 1 for serial debug
+#define POWER_BUTTON 2
+#define FIRE_DS_BUTTON 3
+#define LEFT_LIGHTSABER_PIN 4
+#define RIGHT_LIGHTSABER_PIN 5
+#define BACKLIGHT_PIN 6
+#define DEATHSTAR_BEAM_PIN 7
+// Sound Pins 8-10
+#define MAIN_POWER_RELAY_PIN 11
+#define MARQUEE_RELAY_PIN 12
+#define BUTTON_LED_RELAY_PIN 13
+
 
 //Power On/Off
-enum SYSTEM_STATE {OFF=0, POWERING_ON=1, ON=2, POWERING_OFF=3};
-#define POWER_BUTTON 2
+enum SYSTEM_STATE {OFF=0, POWERING_ON=1, ON=2, POWERING_OFF=3, FIRING_DS = 4};
 #define POWER_HOLD_MS 50
 Bounce powerDebounce = Bounce();
-SYSTEM_STATE powerState = OFF;
+SYSTEM_STATE systemState = OFF;
 
 
-#define FIRE_DS_BUTTON 3
-
-
-//LED Pins
-#define RED_SABER_PIN 4
-#define BLUE_SABER_PIN 5
-
-//Relay Pins
-#define MAIN_POWER_RELAY 11
-#define MARQUEE_RELAY 12
-#define BUTTON_LED_RELAY 13
-
-//LEDs
+//LightSaber LEDs
 #define SABER_LENGTH 50
-CRGB redLightsaber[SABER_LENGTH];
-CRGB blueLightsaber[SABER_LENGTH];
-int saberIndex = 0;
+CRGB leftSaberLEDs[SABER_LENGTH];
+CRGB rightSaberLEDs[SABER_LENGTH];
+LedStrip leftLightSaber = {leftSaberLEDs,SABER_LENGTH};
+LedStrip rightLightSaber = {rightSaberLEDs,SABER_LENGTH};
+FillCommand fillLeftSaberRed = FillCommand();
+FillCommand fillRightSaberBlue = FillCommand();
+FillCommand fillLeftSaberBlack = FillCommand();
+FillCommand fillRightSaberBlack = FillCommand();
+ParallelCommand<3> powerOnCommand = ParallelCommand<3>();
+ParallelCommand<3> powerOffCommand = ParallelCommand<3>();
+
+
+//Sounds
+SoundCommand lightSaberOnSound = SoundCommand(8,2500);
+SoundCommand lightSaberOffSound = SoundCommand(9,1500);
+SoundCommand fireDeathStarSound = SoundCommand(10,22000);
 
 
 //Timing Definitions
-#define LOOP_WAIT_MS 10
+#define LOOP_WAIT_MS 1
 #define FIRE_DS_HOLD_MS 50
+#define SABER_FILL_RATE 20
 
 
 
 Bounce fireDSDebounce = Bounce();
-SoundFx soundPlayer = SoundFx();
 
 
 
@@ -90,7 +72,10 @@ void setup() {
   #endif
   DEBUG_PRINT("Welcome To StarCade!!!");
 
-  soundPlayer.init();
+  for(uint8_t pin = 8; pin <11; pin++){
+    pinMode(pin,OUTPUT);
+    digitalWrite(pin,HIGH);  
+  }
 
   //Setup power button
   pinMode(POWER_BUTTON,INPUT_PULLUP);
@@ -103,17 +88,20 @@ void setup() {
   fireDSDebounce.interval(FIRE_DS_HOLD_MS);
 
   //Setup LEDs
-  FastLED.addLeds<NEOPIXEL, RED_SABER_PIN>(redLightsaber, SABER_LENGTH);
-  FastLED.addLeds<NEOPIXEL, BLUE_SABER_PIN>(blueLightsaber, SABER_LENGTH);
+  FastLED.addLeds<NEOPIXEL, LEFT_LIGHTSABER_PIN>(leftLightSaber.leds, SABER_LENGTH);
+  FastLED.addLeds<NEOPIXEL, RIGHT_LIGHTSABER_PIN>(rightLightSaber.leds, SABER_LENGTH);
 
 
   //Relays
-  pinMode(MAIN_POWER_RELAY,OUTPUT);
-  pinMode(MARQUEE_RELAY,OUTPUT);
-  pinMode(BUTTON_LED_RELAY,OUTPUT);
-  digitalWrite(MAIN_POWER_RELAY,HIGH);
-  digitalWrite(MARQUEE_RELAY,HIGH);
-  digitalWrite(BUTTON_LED_RELAY,HIGH);
+  pinMode(MAIN_POWER_RELAY_PIN,OUTPUT);
+  pinMode(MARQUEE_RELAY_PIN,OUTPUT);
+  pinMode(BUTTON_LED_RELAY_PIN,OUTPUT);
+  digitalWrite(MAIN_POWER_RELAY_PIN,HIGH);
+  digitalWrite(MARQUEE_RELAY_PIN,HIGH);
+  digitalWrite(BUTTON_LED_RELAY_PIN,HIGH);
+
+  setupAnimations();
+
 
 }
 
@@ -121,89 +109,80 @@ void setup() {
 void loop() {
   processPowerButton();
   processFireDS();
-  soundPlayer.update();
   delay(LOOP_WAIT_MS);
 }
 
 
 
+void setupAnimations(){
+  // Power On
+  fillLeftSaberRed.init(leftLightSaber,CRGB::Red,SABER_FILL_RATE,FillCommand::Direction::FORWARD);
+  fillRightSaberBlue.init(rightLightSaber,CRGB::Blue,SABER_FILL_RATE,FillCommand::Direction::FORWARD);
+  powerOnCommand.add(&fillLeftSaberRed).add(&fillRightSaberBlue).add(&lightSaberOnSound);
+  
+  //Power Off
+  fillLeftSaberBlack.init(leftLightSaber,CRGB::Black,SABER_FILL_RATE,FillCommand::Direction::REVERSE);
+  fillRightSaberBlack.init(rightLightSaber,CRGB::Black,SABER_FILL_RATE,FillCommand::Direction::REVERSE);
+  powerOffCommand.add(&fillLeftSaberBlack).add(&fillRightSaberBlack).add(&lightSaberOffSound);
+}
+
 
 void processPowerButton(){
-  bool lightSabersDone = false;
   powerDebounce.update(); 
-  switch(powerState){
+  switch(systemState){
     case OFF:
       if(powerDebounce.fell()){
         DEBUG_PRINT("Powering on...");
-        digitalWrite(MAIN_POWER_RELAY,LOW);
-        digitalWrite(MARQUEE_RELAY,LOW);
-        digitalWrite(BUTTON_LED_RELAY,LOW);
-        soundPlayer.playSound(SoundFx::SABER_ON);
-        saberIndex = 0;
-        powerState = POWERING_ON;
+        digitalWrite(MAIN_POWER_RELAY_PIN,LOW);
+        digitalWrite(MARQUEE_RELAY_PIN,LOW);
+        digitalWrite(BUTTON_LED_RELAY_PIN,LOW);
+        powerOnCommand.begin();
+        systemState = POWERING_ON;
       }
       break;
     case POWERING_ON:
-      lightSabersDone = processLightsaberOn();
+      powerOnCommand.update();
       FastLED.show();
-      if(lightSabersDone){
+      if(powerOnCommand.isDone()){
         DEBUG_PRINT("System ON");
-        powerState = ON;
+        systemState = ON;
       }
       break;
     case ON:
       if(powerDebounce.fell()){
         DEBUG_PRINT("Powering off...");
-        digitalWrite(MAIN_POWER_RELAY,HIGH);
-        digitalWrite(MARQUEE_RELAY,HIGH);
-        digitalWrite(BUTTON_LED_RELAY,HIGH);
-        soundPlayer.playSound(SoundFx::SABER_OFF);
-        saberIndex = SABER_LENGTH-1;
-        powerState = POWERING_OFF;
+        digitalWrite(MAIN_POWER_RELAY_PIN,HIGH);
+        digitalWrite(MARQUEE_RELAY_PIN,HIGH);
+        digitalWrite(BUTTON_LED_RELAY_PIN,HIGH);
+        powerOffCommand.begin();
+        systemState = POWERING_OFF;
       }
       break;
     case POWERING_OFF:
-      lightSabersDone = processLightsaberOff();
+      powerOffCommand.update();
       FastLED.show();
-      if(lightSabersDone){
+      if(powerOffCommand.isDone()){
         DEBUG_PRINT("System OFF");
-        powerState = OFF;
+        systemState = OFF;
       }
+    default:
       break;
   }
-}
-
-
-bool processLightsaberOn(){
- if(saberIndex < SABER_LENGTH){
-    redLightsaber[saberIndex] = CRGB::Red;
-    blueLightsaber[saberIndex] = CRGB::Blue;
-    saberIndex++;
-    return false;
-  }
-  return true;
-}
-
-
-
-
-
-bool processLightsaberOff(){
- if(saberIndex >= 0){
-    redLightsaber[saberIndex] = CRGB::Black;
-    blueLightsaber[saberIndex] = CRGB::Black;
-    saberIndex--;
-    return false;
-  }
-  return true;
 }
 
 
 void processFireDS(){
   fireDSDebounce.update();
-  if(fireDSDebounce.fell()){
+  if(systemState == ON && fireDSDebounce.fell()){
     DEBUG_PRINT("Fire Death Star!!!!"); 
-    soundPlayer.playSound(SoundFx::FIRE_DS);
+    fireDeathStarSound.begin();
+    systemState = FIRING_DS;
+  }else if(systemState == FIRING_DS){
+    fireDeathStarSound.update();
+    if(fireDeathStarSound.isDone()){
+      DEBUG_PRINT("Alderaan has been destroyed!"); 
+      systemState = ON;
+    }
   }
 }
 
